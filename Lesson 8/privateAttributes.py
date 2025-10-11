1f08f51d43d1f5353a34309cc6fd9ac234df1e20
Labeat-b <labeat.bytyqi@gmail.com>
2025-10-11 14:41:15 +0200

    Inheritance

--- Lesson 8/privateAttributes.py
+++ Lesson 8/privateAttributes.py
@@ -1,6 +1,6 @@
 class MyClass:
     def __init__(self):
-        self.__private_varibale = "This is a private variable"
+        self.__private_variable = "This is a private variable"
     def __private_method(self):
         print("This is a private method")
 
@@ -8,4 +8,3 @@ my_class = MyClass()
 
 #Error attributes
 print(my_class.__private_variable)
-my_class.__private_method
---
+++ Lesson 9/animal2.py
@@ -0,0 +1,51 @@
+class Animal:
+    def __init__(self, name):
+        self.name = name
+
+    def sound(self):
+        print("Some generic animal sound")
+
+    def description(self):
+        print(f"This animal is named {self.name}")
+
+class Dog(Animal):
+    def __init__(self, name, breed):
+        super().__init__(name)
+        self.breed = breed
+
+    def sound(self):
+        print("Woof, Woof")
+
+    def description(self):
+        super().description()
+        print(f"Breed: {self.breed}")
+
+class Cat(Animal):
+    def __init__(self, name, color):
+        super().__init__(name)
+        self.color = color
+
+    def sound(self):
+        print("Meow, Meow")
+
+    def description(self):
+        super().description()
+        print(f"Color: {self.color}")
+
+animal= Animal("Generic Animal")
+animal.sound()
+animal.description()
+
+
+dog = Dog("Max", "Husky")
+dog.sound()
+dog.description()
+print(dog.description())
+
+cat = Cat("Max", "Green")
+cat.sound()
+cat.description()
+print(cat.description())
+
+
+
